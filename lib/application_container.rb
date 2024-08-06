8bc3173da60f92bab181901e5d617fd68fe4ea41
Alexander Veselov <collabsis.g@gmail.com>
2024-08-06 09:45:29 +0400

    Fixed Zeitwerk::NameError

--- lib/application_container.rb
+++ lib/application_container.rb
@@ -4,8 +4,8 @@ class ApplicationContainer
   extend Dry::Container::Mixin
 
   if Rails.env.test?
-    register :octokit_client, -> { OctokitClientStub }
-    register :repository_check_workflow_stub, -> { RepositoryCheckWorkflowStub }
+    register(:octokit_client) { OctokitClientStub }
+    register(:repository_check_workflow_stub) { RepositoryCheckWorkflowStub }
   else
     register(:octokit_client) { Octokit::Client }
     register(:repository_check_workflow) { RepositoryCheckWorkflow }
