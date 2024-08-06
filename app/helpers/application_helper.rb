dd5e1161a6b41af4dc9f1446b1c85ea5bebdab55
Alexander Veselov <collabsis.g@gmail.com>
2024-08-06 08:21:59 +0400

    WIP: fix translates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def localized_check_state(state)
-    I18n.t("activerecord.attributes.check.states.#{state}")
+    I18n.t("activerecord.attributes.repository/check.states.#{state}")
   end
 end
